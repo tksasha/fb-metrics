8ea168bc20496f605c99584dea033169d16df4cd
Alexander Tkachuk <mail@tksasha.me>
2017-07-04 17:38:35 +0300

    added creating metrics for Page

--- app/factories/metric_factory.rb
+++ app/factories/metric_factory.rb
@@ -10,7 +10,17 @@ class MetricFactory
   end
 
   def create
-    puts links
+    links.map do |link|
+      page = site.pages.find_or_create_by url: link.to_s
+
+      shares_count = Shares.count link.to_s
+
+      if metric = page.metric
+        metric.update shares_count: shares_count
+      else
+        page.create_metric shares_count: shares_count
+      end
+    end
   end
 
   private
@@ -19,7 +29,7 @@ class MetricFactory
   end
 
   def html
-    Nokogiri::HTML(open "#{ scheme }://#{ host }")
+    Nokogiri::HTML(open uri.to_s)
   end
 
   def links
@@ -31,9 +41,13 @@ class MetricFactory
       # like `/companies.html`
       #
       when href.host.nil? && href.scheme.nil?
+        href.scheme = scheme
+
+        href.host = host
+
         href
       #
-      # like `mailto:mail@tksasha.me`
+      # avoid links like `mailto:mail@tksasha.me`
       #
       when href.host.nil? && href.scheme.present? && href.scheme =~ URIParser::SCHEMES
         href
@@ -43,9 +57,15 @@ class MetricFactory
       # where `www.tksasha.me` is the same host
       #
       when href.host == host && href.scheme.present? && href.scheme =~ URIParser::SCHEMES
+        href.path = '/' if href.path.empty?
+
         href
       end
     end.compact
+  end
+
+  def site
+    @site ||= Site.find_or_create_by url: host
   end
 
   class << self
--- spec/factories/metric_factory_spec.rb
+++ spec/factories/metric_factory_spec.rb
@@ -25,6 +25,7 @@ RSpec.describe MetricFactory do
             <a href="http://www.rubyonrails.org/">Ruby on Rails</a>
             <a href="mailto:mail@tksasha.me">My Email</a>
             <a href="/posts.html">Posts</a>
+            <a href="http://www.tksasha.me">Root</a>
           </body>
         </html>
       eos
@@ -34,16 +35,61 @@ RSpec.describe MetricFactory do
 
     its(:links) do
       should eq [
-        URI('/'),
-        URI('/companies.html'),
-        URI('/companies.html?param=true'),
-        URI('http://www.tksasha.me/companies.html'),
-        URI('/posts.html')
+        URI::Generic.build(scheme: 'http', host: 'www.tksasha.me', path: '/'),
+        URI::Generic.build(scheme: 'http', host: 'www.tksasha.me', path: '/companies.html'),
+        URI::Generic.build(scheme: 'http', host: 'www.tksasha.me', path: '/companies.html', query: 'param=true'),
+        URI::HTTP.build(scheme: 'http', host: 'www.tksasha.me', path: '/companies.html'),
+        URI::Generic.build(scheme: 'http', host: 'www.tksasha.me', path: '/posts.html'),
+        URI::HTTP.build(scheme: 'http', host: 'www.tksasha.me', path: '/')
       ]
     end
   end
 
+  describe '#site' do
+    before { expect(Site).to receive(:find_or_create_by).with(url: 'www.tksasha.me').and_return(:site) }
+
+    its(:site) { should eq :site }
+  end
+
   describe '#create' do
+    let(:link) { URI.parse 'www.tksasha.me/companies.html' }
+
+    let(:site) { stub_model Site }
+
+    let(:page) { stub_model Page }
+
+    before { expect(subject).to receive(:links).and_return([link]) }
+
+    before { expect(subject).to receive(:site).and_return(site) }
+
+    before { expect(Shares).to receive(:count).with(link.to_s).and_return(69) }
+
+    before do
+      #
+      # site.pages.find_or_create_by(url: link.to_s) -> page
+      #
+      expect(site).to receive(:pages) do
+        double.tap { |a| expect(a).to receive(:find_or_create_by).with(url: link.to_s).and_return(page) }
+      end
+    end
+
+    context do
+      before { expect(page).to receive(:metric).and_return(nil) }
+
+      before { expect(page).to receive(:create_metric).with(shares_count: 69) }
+
+      it { expect { subject.create }.to_not raise_error }
+    end
+
+    context do
+      let(:metric) { stub_model Metric }
+
+      before { expect(page).to receive(:metric).and_return(metric) }
+
+      before { expect(metric).to receive(:update).with(shares_count: 69) }
+
+      it { expect { subject.create }.to_not raise_error }
+    end
   end
 
   describe '.create' do
